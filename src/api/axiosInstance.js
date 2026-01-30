76b1ae5d49f4f93a4b698ffd14a05a10197b7b9d
Osman YILMAZ <osman.ylmz6126@gmail.com>
2026-01-30 12:35:14 +0300

    Fix the issue for vercel

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const isProd = import.meta.env.PROD;
+
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: isProd ? "" : "http://localhost:8080",
 });
 
 axiosInstance.interceptors.request.use((config) => {
